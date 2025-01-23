e7822c32291ab31fa2dd18ff88836798d0dfbcfc
Traikia Sid Ahmed <gs_traikia@esi.dz>
2025-01-23 08:53:28 +0100

    Minor changes

--- python/Math/Triangle Quest/Solution.py
+++ python/Math/Triangle Quest/Solution.py
@@ -1,6 +1,6 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
-# Iterate over the range from 1 to the input number
-for i in range(1, int(input()) + 1):
-    # Calculate the square of the repeated digit number (1, 11, 111, 1111, ...)
-    print(((pow(10, i) - 1) // 9) ** 2)
+# Iterate over the range from 1 to the input number (exclusive)
+for i in range(1, int(input())):
+    # Calculate and print the value for the current iteration
+    print(i * (((10 ** i) - 1) // 9))
